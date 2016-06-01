40b662d74de59cb967aa90fcb851d2e630a91a4c
stankiewicz <rrydziu@gmail.com>
2016-06-01 23:28:21 +0200

    make it better

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
     app = connexion.App(__name__)
-    app_host = os.environ.get("HEROKU_APP_NAME", "127.0.0.1:8080")
+    heroku_app = os.environ.get("HEROKU_APP_NAME")
+    app_host = (heroku_app +'.herokuapp.com') if heroku_app else "127.0.0.1:8080"
     app.add_api('api.yaml', arguments={'host': app_host}, resolver=RestyResolver('api'))
     app.run(server='tornado', port=int(os.environ.get("PORT", 8080)))
